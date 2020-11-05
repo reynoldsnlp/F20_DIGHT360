8dd3d786731e2579ad0f806c7c8a06e732e87717
reynoldsnlp <reynoldsnlp@users.noreply.github.com>
2020-11-04 19:41:12 -0700

    add StratifiedKFold to RFECV

--- activities/RFECV.py
+++ activities/RFECV.py
@@ -1,6 +1,7 @@
 import pandas
 from sklearn.ensemble import ExtraTreesClassifier
 from sklearn.feature_selection import RFECV
+from sklearn.model_selection import StratifiedKFold
 
 
 with open('mc_features.csv') as mc_file:
@@ -13,7 +14,8 @@ labels = array[:, -1]
 # Feature ranking with Recursive Feature Elimination and Cross-Validated
 # selection of the best number of features.
 ETC = ExtraTreesClassifier(n_estimators=10)
-selector = RFECV(ETC, step=1, cv=10)
+skf = StratifiedKFold(10, shuffle=True, random_state=42)
+selector = RFECV(ETC, step=1, cv=skf)
 selector = selector.fit(feats, labels)
 # In the following line, remember that iterating over a pandas DataFrame gives
 # you just the column names, i.e. the names of the features
